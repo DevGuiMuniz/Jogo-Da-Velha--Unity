c46e64ee8b4894e7610c4e163c1d0c5c5e2cbf48
DevGuiMuniz <guigugui16@gmail.com>
2024-04-15 18:38:08 -0300

    add arquivos atualizados

--- Assets/gameControl.cs
+++ Assets/gameControl.cs
@@ -14,101 +14,95 @@ public class gameControl : MonoBehaviour
     public Text Text7;
     public Text Text8;
     public Text Text9;
-     public Text[] texts;
+    public Text[] texts;
 
     public bool turnX = true;
     public Text messageText;
 
     public void ClickButton1()
     {
-       if (turnX && Text1.text == "")
+        if (turnX && Text1.text == "")
         {
-            Text1.text = "X";
+            Text1.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
 
     public void ClickButton2()
     {
-      if (turnX && Text2.text == "")
+        if (turnX && Text2.text == "")
         {
-            Text2.text = "X";
+            Text2.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-        
     }
 
     public void ClickButton3()
     {
         if (turnX && Text3.text == "")
         {
-            Text3.text = "X";
+            Text3.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-        
-        
-
     }
+
     public void ClickButton4()
     {
         if (turnX && Text4.text == "")
         {
-            Text4.text = "X";
+            Text4.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
+
     public void ClickButton5()
     {
         if (turnX && Text5.text == "")
         {
-            Text5.text = "X";
+            Text5.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
 
     public void ClickButton6()
     {
         if (turnX && Text6.text == "")
         {
-            Text6.text = "X";
+            Text6.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
+
     public void ClickButton7()
     {
-      if (turnX && Text7.text == "")
+        if (turnX && Text7.text == "")
         {
-            Text7.text = "X";
+            Text7.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
+
     public void ClickButton8()
     {
-
-       if (turnX && Text8.text == "")
+        if (turnX && Text8.text == "")
         {
-            Text8.text = "X";
+            Text8.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
     }
@@ -117,52 +111,61 @@ public class gameControl : MonoBehaviour
     {
         if (turnX && Text9.text == "")
         {
-            Text9.text = "X";
+            Text9.text = "★";
             turnX = false;
-            messageText.text = "Jogador: O";
+            messageText.text = "Jogador: ♥";
             VictoryCheck();
         }
-
     }
 
-    public void VictoryCheck()
-{
-     if ((Text1.text == "X" && Text5.text == "X" && Text9.text == "X") ||
-        (Text3.text == "X" && Text5.text == "X" && Text7.text == "X") ||
-        (Text1.text == "X" && Text2.text == "X" && Text3.text == "X") ||
-        (Text4.text == "X" && Text5.text == "X" && Text6.text == "X") ||
-        (Text7.text == "X" && Text8.text == "X" && Text9.text == "X") ||
-        (Text1.text == "X" && Text4.text == "X" && Text7.text == "X") ||
-        (Text2.text == "X" && Text5.text == "X" && Text8.text == "X") ||
-        (Text3.text == "X" && Text6.text == "X" && Text9.text == "X"))
+    public void AIMove()
     {
-        messageText.text = "O jogador X venceu!";
-    }
-    else if ((Text1.text == "O" && Text5.text == "O" && Text9.text == "O") ||
-             (Text3.text == "O" && Text5.text == "O" && Text7.text == "O") ||
-             (Text1.text == "O" && Text2.text == "O" && Text3.text == "O") ||
-             (Text4.text == "O" && Text5.text == "O" && Text6.text == "O") ||
-             (Text7.text == "O" && Text8.text == "O" && Text9.text == "O") ||
-             (Text1.text == "O" && Text4.text == "O" && Text7.text == "O") ||
-             (Text2.text == "O" && Text5.text == "O" && Text8.text == "O") ||
-             (Text3.text == "O" && Text6.text == "O" && Text9.text == "O"))
-    {
-        messageText.text = "O jogador O venceu!";
-    }
-    else if (Text1.text != "" && Text2.text != "" && Text3.text != "" &&
-             Text4.text != "" && Text5.text != "" && Text6.text != "" &&
-             Text7.text != "" && Text8.text != "" && Text9.text != "")
-    {
-        messageText.text = "O jogo empatou";
-    }
-}
+        // Estratégia para bloquear as jogadas do jogador
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (texts[i].text == "")
+            {
+                // Simula a jogada do jogador e verifica se o jogador ganharia na próxima jogada
+                texts[i].text = "♥";
+                if (CheckForWin("♥"))
+                {
+                    texts[i].text = "♥"; // Bloqueia a jogada do jogador
+                    turnX = true;
+                    messageText.text = "Jogador: ★";
+                    VictoryCheck();
+                    return;
+                }
+                else
+                {
+                    texts[i].text = ""; // Desfaz a simulação
+                }
+            }
+        }
 
- void AIMove()
-    {
-       // Lista para armazenar os índices das células vazias
+        // Estratégia para buscar uma vitória iminente
+        for (int i = 0; i < texts.Length; i++)
+        {
+            if (texts[i].text == "")
+            {
+                // Simula a jogada da IA e verifica se ela ganharia na próxima jogada
+                texts[i].text = "★";
+                if (CheckForWin("★"))
+                {
+                    texts[i].text = "♥"; // Faz a jogada da IA para ganhar o jogo
+                    turnX = true;
+                    messageText.text = "Jogador: ★";
+                    VictoryCheck();
+                    return;
+                }
+                else
+                {
+                    texts[i].text = ""; // Desfaz a simulação
+                }
+            }
+        }
+
+        // Se nenhuma estratégia for possível, escolhe uma posição aleatória
         List<int> emptyCellsIndexes = new List<int>();
-
-        // Encontra as células vazias
         for (int i = 0; i < texts.Length; i++)
         {
             if (texts[i].text == "")
@@ -171,37 +174,62 @@ public class gameControl : MonoBehaviour
             }
         }
 
-        // Verifica se existem células vazias para jogar
         if (emptyCellsIndexes.Count > 0)
         {
-            // Escolhe aleatoriamente um índice das células vazias
             int randomIndex = Random.Range(0, emptyCellsIndexes.Count);
             int cellIndex = emptyCellsIndexes[randomIndex];
-
-            // Coloca "O" na célula escolhida
-            texts[cellIndex].text = "O";
+            texts[cellIndex].text = "♥";
             turnX = true;
-            messageText.text = "Jogador: X";
-
+            messageText.text = "Jogador: ★";
             VictoryCheck();
         }
     }
 
-    // Start is called before the first frame update
+    public bool CheckForWin(string player)
+    {
+        if ((Text1.text == player && Text5.text == player && Text9.text == player) ||
+            (Text3.text == player && Text5.text == player && Text7.text == player) ||
+            (Text1.text == player && Text2.text == player && Text3.text == player) ||
+            (Text4.text == player && Text5.text == player && Text6.text == player) ||
+            (Text7.text == player && Text8.text == player && Text9.text == player) ||
+            (Text1.text == player && Text4.text == player && Text7.text == player) ||
+            (Text2.text == player && Text5.text == player && Text8.text == player) ||
+            (Text3.text == player && Text6.text == player && Text9.text == player))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void VictoryCheck()
+    {
+        if (CheckForWin("★"))
+        {
+            messageText.text = "O jogador ★ venceu!";
+        }
+        else if (CheckForWin("♥"))
+        {
+            messageText.text = "O jogador ♥ venceu!";
+        }
+        else if (Text1.text != "" && Text2.text != "" && Text3.text != "" &&
+                 Text4.text != "" && Text5.text != "" && Text6.text != "" &&
+                 Text7.text != "" && Text8.text != "" && Text9.text != "")
+        {
+            messageText.text = "O jogo empatou";
+        }
+    }
+
     void Start()
     {
         texts = new Text[] { Text1, Text2, Text3, Text4, Text5, Text6, Text7, Text8, Text9 };
-        messageText.text = "Jogador: X"; 
-      
+        messageText.text = "Jogador: ★";
     }
 
-    // Update is called once per frame
     void Update()
     {
-                  if (!turnX)
+        if (!turnX)
         {
             AIMove();
-        }
-  
-    }
+        }
+    }
 }
